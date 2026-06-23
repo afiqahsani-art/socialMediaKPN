7fdde32293b2a05efa71e8b7a4752d9150ea1770
Kamal Norizan <kamalnorizan@gmail.com>
2026-06-23 16:04:01 +0800

    login using breeze and vue

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\TestController;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
-// DB::listen(function ($event) {
-//     dump($event->sql);
-// });
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('helloworld', [TestController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('posts', [PostController::class, 'index']);
+require __DIR__.'/auth.php';
