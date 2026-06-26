3f677f36bfdd69f0677a92ad45f6859b4c11c02f
afiqah <afiqahsani@perpaduan.gov.my>
2026-06-26 16:33:54 +0800

    migrate add soft delete to posts

--- database/migrations/2026_06_26_075933_add_soft_delete_to_posts.php
+++ database/migrations/2026_06_26_075933_add_soft_delete_to_posts.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->softDeletes(); // Add soft delete column
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes(); // Remove soft delete column
         });
     }
 };
